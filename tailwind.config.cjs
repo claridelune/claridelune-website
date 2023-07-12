d3a0e62696888c7b7905cf5d39ef181aa128fed9
cclari <camilaclary@gmail.com>
2023-07-12 14:01:37 -0500

    theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            synthwave: {
+                'background': '#2a2139',
+                'primary': '#f8f8ff',
+                'secondary': '#5535a2',
+                'blue': '#2a3865',
+            },
+        },
+    },
   },
   plugins: [
       require('@tailwindcss/typography')
